776c6338a2f065c0ebb813d7434f6ac4833188bf
Cetin Guven <117115603+CetinGuven@users.noreply.github.com>
2023-01-16 17:58:23 +0300

    BikeChain

---
+++ BikeChain.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract BikeChain {
+
+    address owner;
+
+    constructor () {
+        owner =msg.sender;
+    }
+ 
+    //Add yourself as a Renter
+
+    struct Renter {
+        address payable walletAddress;
+        string firstName;
+        string lastName;
+        bool canRent;
+        bool active;
+        uint balance;
+        uint due;
+        uint start;
+        uint end;
+    } 
+    mapping (address => Renter) public renters;
+
+    function addRenter (  address payable walletAddress, string memory firstName, string memory lastName,
+        bool canRent, bool active, uint balance, uint due, uint start, uint end ) public {
+            renters[walletAddress]=Renter(walletAddress,firstName,lastName,canRent,active,balance,due,start,end);
+        }
+
+    //Checkout bike
+
+    function checkOut(address walletAddress) public {
+        renters[walletAddress].active = true;
+        renters[walletAddress].start = block.timestamp;
+        renters[walletAddress].canRent = false;
+    }
+    //Check in a bike
+
+    function checkIn(address walletAddress) public {
+         renters[walletAddress].active = false;
+         renters[walletAddress].end = block.timestamp;
+         setDue(walletAddress);
+     }
+
+    //Get total duration of bike use
+    function renterTimespan ( uint start, uint end) internal pure returns (uint) {
+        return end- start;
+    }
+
+    function getTotalDuration (address walletAddress) public view returns (uint) {
+        uint timespan = renterTimespan ( renters[walletAddress].start, renters[walletAddress].end);
+        uint timespanInMinutes = timespan /60;
+        return timespanInMinutes;
+    }
+
+    //Get Contract balance
+    function balanceOf () view public returns (uint) {
+        return address(this).balance;
+    }
+
+    //Get Renter's balance
+    function balanceOfRenter (address walletAddress) public view returns (uint) {
+        return renters[walletAddress].balance;
+    }
+
+    //Set Due amount
+     function  setDue(address walletAddress) internal {
+         uint timespanMinutes = getTotalDuration(walletAddress);
+         uint fiveMinuteIncrements = timespanMinutes /5;
+         renters [walletAddress].due = fiveMinuteIncrements*5000000000000000;
+     }
+
+    function canRentBike (address walletAddress) public view returns (bool) {
+        return renters[walletAddress].canRent;
+    }
+
+    // Deposit
+    function deposit (address walletAddress) payable public {
+        renters[walletAddress].balance += msg.value;
+    }
+
+    //Make Payment
+
+
+
+}
